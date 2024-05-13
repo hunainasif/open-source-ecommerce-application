ec97d6e8ce563fc3e039dc1fc6a43161c0510ae1
Hunain Asif <hunainasif960@gmail.com>
2024-05-13 19:56:55 +0500

    Commit the Code

--- src/app/(products)/[category]/[subCategory]/[singleItem]/page.tsx
+++ src/app/(products)/[category]/[subCategory]/[singleItem]/page.tsx
@@ -1,11 +1,70 @@
-import React from 'react'
-
+"use client"
+import React, { useState } from 'react'
+import styles from "./singleItem.module.css"
+import { CheckCircleOutline, CurrencyPound, FavoriteBorder, ShoppingCart  } from '@mui/icons-material'
 export default function SingleItem({params}:{params:{category:string,subCategory:string,singleItem:string}}) {
+  // {params.category}
+  // {params.subCategory}
+  // {params.singleItem}
+
+  const [index,setIndex]=useState(0)
   return (
-    <div>
-      {params.category}
-      {params.subCategory}
-      {params.singleItem}
+    <div className={styles.singleItem}>
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.left}>
+            <h1>Hey i am Left</h1>
+          </div>
+          <div className={styles.right}>
+             <div className={styles.top}>
+              <span className="title">Timberland Ankle Classic Boots</span>
+              <span className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere expedita aperiam tempora, temporibus quod amet incidunt. Aut molestias perferendis consequatur sapiente quas. Officiis temporibus porro consequatur facere inventore, sint ducimus?</span>
+              
+              <FavoriteBorder/>
+             </div>
+             <div className="center">
+              <div className="money">
+
+              <CurrencyPound/>
+              <span className="price">129</span>
+              <span>.00</span>
+              </div>
+
+              <div className="sizes">
+                <select name="" id="">
+                  <option value="">X-SMALL</option>
+                  <option value="">SMALL</option>
+                  <option value="">Medium</option>
+                  <option value="">LARGE</option>
+                  <option value="">2XL</option>
+                  <option value="">3XL</option>
+                </select>
+              </div>
+              <div className="quantity">
+              <button onClick={() => { setIndex(prev => prev !== 0 ? prev - 1 : prev) }}> - </button>
+
+                {index}
+                <button onClick={()=>{setIndex(prev=>prev+1)}}>+</button>
+              </div>
+
+              
+             </div>
+
+             <div className="bottom">
+               <div className="cart">
+                <ShoppingCart/>
+                Add To Cart
+               </div>
+
+               <div className="checkOut">
+                <CheckCircleOutline/>
+                CheckOut
+               </div>
+             </div>
+          </div>
+        </div>
+      </div>
+     
     </div>
   )
 }
--- src/app/(products)/[category]/page.tsx
+++ src/app/(products)/[category]/page.tsx
@@ -4,36 +4,100 @@ import Image from 'next/image'
 import Link from 'next/link'
 export default function Category({ params }: { params: { category: string } }) {
   
-  const links=[
+  const menLinks=[
      {
       id:1,
       title:"Shirts",
-      img:"/img/slider2.jpg"
+      img:"/img/men_shirt.jpg"
      },
      {
       id:2,
       title:"Bottoms",
-      img:"/img/slider2.jpg"
+      img:"/img/men_bottom.jpg"
      },
      {
       id:3,
       title:"Jackets",
-      img:"/img/slider2.jpg"
+      img:"/img/men_jacket.jpg"
      },
      {
       id:4,
       title:"Shoes",
-      img:"/img/slider2.jpg"
+      img:"/img/men_shoes.jpg"
      },
      {
       id:5,
       title:"SkinCare",
-      img:"/img/slider2.jpg"
+      img:"/img/men_skincare.jpg"
      },
      {
       id:6,
       title:"Formals",
-      img:"/img/slider2.jpg"
+      img:"/img/men_formals.jpg"
+     },
+  ]
+  const  womenLinks=[
+     {
+      id:1,
+      title:"Shirts",
+      img:"/img/women_shirt.jpg"
+     },
+     {
+      id:2,
+      title:"Bottoms",
+      img:"/img/women_bottom.jpg"
+     },
+     {
+      id:3,
+      title:"Jackets",
+      img:"/img/women_jacket.jpg"
+     },
+     {
+      id:4,
+      title:"Shoes",
+      img:"/img/women_shoes.jpg"
+     },
+     {
+      id:5,
+      title:"SkinCare",
+      img:"/img/women_skin.jpg"
+     },
+     {
+      id:6,
+      title:"Formals",
+      img:"/img/women_formals.jpg"
+     },
+  ]
+  const kidsLinks=[
+     {
+      id:1,
+      title:"Shirts",
+      img:"/img/men_shirt.jpg"
+     },
+     {
+      id:2,
+      title:"Bottoms",
+      img:"/img/men_bottom.jpg"
+     },
+     {
+      id:3,
+      title:"Jackets",
+      img:"/img/men_jacket.jpg"
+     },
+     {
+      id:4,
+      title:"Shoes",
+      img:"/img/men_shoes.jpg"
+     },
+     {
+      id:5,
+      title:"SkinCare",
+      img:"/img/women_skin.jpg"
+     },
+     {
+      id:6,
+      title:"Formals",
+      img:"/img/men_formals.jpg"
      },
   ]
   return (
@@ -41,18 +105,40 @@ export default function Category({ params }: { params: { category: string } }) {
       <h1 className={styles.heading}>{params.category.toUpperCase()}</h1>
        <div className={styles.container}>
 
-          {links.map((item)=>(
-        <div className={styles.item} key={item.id}>
-             <div className={styles.imageContainer}>
-            <Image src={item.img} alt="" layout="fill" className={styles.img} />
-          </div>
-           <div className={styles.textContainer}>
-
-            <Link href={(`/${params.category}/${item.title}`).toLowerCase()} className={styles.text}>{item.title}</Link>
-           </div>
-         
-        </div>
-          ))}
+       {
+    params.category === "men" ? 
+        menLinks.map((item) => (
+            <div className={styles.item} key={item.id}>
+                <div className={styles.imageContainer}>
+                    <Image src={item.img} alt="" layout="fill" className={styles.img} />
+                </div>
+                <div className={styles.textContainer}>
+                    <Link href={`/${params.category}/${item.title}`.toLowerCase()} className={styles.text}>{item.title}</Link>
+                </div>
+            </div>
+        )) : 
+        params.category === "women" ? 
+            womenLinks.map((item) => (
+                <div className={styles.item} key={item.id}>
+                    <div className={styles.imageContainer}>
+                        <Image src={item.img} alt="" layout="fill" className={styles.img} />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <Link href={`/${params.category}/${item.title}`.toLowerCase()} className={styles.text}>{item.title}</Link>
+                    </div>
+                </div>
+            )) : 
+            kidsLinks.map((item) => (
+                <div className={styles.item} key={item.id}>
+                    <div className={styles.imageContainer}>
+                        <Image src={item.img} alt="" layout="fill" className={styles.img} />
+                    </div>
+                    <div className={styles.textContainer}>
+                        <Link href={`/${params.category}/${item.title}`.toLowerCase()} className={styles.text}>{item.title}</Link>
+                    </div>
+                </div>
+            ))
+}
           
        </div>
     </div>
