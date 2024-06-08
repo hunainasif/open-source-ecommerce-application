9e746bca59f0600878986182976783d277232474
Hunain Asif <hunainasif960@gmail.com>
2024-06-08 13:50:38 +0500

    Backend Started

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 export default function Checkout() {
   return (
-    <div>
-      <h1>Hey I am The Checkout</h1>
+    <div className="checkOut">
+      <h1 className="mainHeading">Checkout</h1>
+      <div className="container">
+        <form action="">
+          <div className="form_control">
+            <label htmlFor="">Name</label>
+            <input type="text" name="" id="" />
+          </div>
+          <div className="form_control">
+            <label htmlFor="">Email</label>
+            <input type="email" name="" id="" />
+          </div>
+          <div className="form_control">
+            <label htmlFor="">Address</label>
+            <input type="text" name="" id="" />
+          </div>
+          <div className="form_control">
+            <label htmlFor="">Contact Number</label>
+            <input type="tel" name="" id="" />
+          </div>
+        </form>
+        <div className="payments">
+          <button>Pay</button>
+        </div>
+      </div>
     </div>
   )
 }
